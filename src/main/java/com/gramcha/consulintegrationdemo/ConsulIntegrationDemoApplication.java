17192350d7716f7e30f2a323dd671d5bc60b2b0b
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-06-19 12:19:45 +0530

    springboot biolerplate

---
+++ src/main/java/com/gramcha/consulintegrationdemo/ConsulIntegrationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.gramcha.consulintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulIntegrationDemoApplication.class, args);
+	}
+}
